651f20a703ecb2f4fd816fe23ee922066e7a0101
nizutaro <niduka81@gmail.com>
2020-10-09 19:54:43 +0900

    s3実装

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -33,7 +33,9 @@ class ImagesController < ApplicationController
   def show
     @image = Image.find(params[:id])
     # 参照先のS3オブジェクトURLを作成
-    @image_url = "https://image-files-resize-niduka.s3-ap-northeast-1.amazonaws.com/store/" + @image.image_id + "-thumbnail."
+    @image_url = "https://image-files-original-niduka.s3-ap-northeast-1.amazonaws.com/store/" + @image.image_id 
+    #+ "-thumbnail."
+    #将来resizeを参照させたい
   end
 
   def destroy
